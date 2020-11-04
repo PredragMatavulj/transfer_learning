d7ddd0811c2f76e2062b42fd91e8e78b28d842c7
sljelic@gmail.com <dijadema2016>
2020-11-04 22:00:22 +0100

    parallel

--- Rapid-E/src/experiment.py
+++ Rapid-E/src/experiment.py
@@ -294,8 +294,7 @@ class Experiment:
                 #train_batch_target = torch.tensor(list(map(lambda x: x[1], train_batch))).to(device)
                 #train_batch_weights = torch.tensor(list(map(lambda x: x[2], train_batch))).to(device)
                 
-                train_batch_target = torch.tensor(list(map(lambda x: x[1], train_batch))).cuda(non_blocking=True)
-                train_batch_weights = torch.tensor(list(map(lambda x: x[2], train_batch))).cuda(non_blocking=True)
+                
                 #print('Data:')
                 #print(train_batch_target)
                 #print(train_batch_weights)
@@ -303,6 +302,8 @@ class Experiment:
                 
                 train_batch_output = self.model(scatters, spectrums, lifetimes1, lifetimes2, sizes)
                 #print(objective_batch_loss)
+                train_batch_target = torch.tensor(list(map(lambda x: x[1], train_batch))).cuda(non_blocking=True)
+                train_batch_weights = torch.tensor(list(map(lambda x: x[2], train_batch))).cuda(non_blocking=True)
                 
                 objective_batch_loss = self.criteria['objective_criteria'](train_batch_output, train_batch_target, train_batch_weights)
                 
